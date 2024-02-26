b3fbbeb48f0121be15511946ee10c86b5cc7bac7
Braulio <braulioandre12@gmail.com>
2024-02-26 20:23:34 +0200

    added embedded Looker Studio report for suggestion

---
+++ src/pages/dashboard/embedded.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EmbeddedReport = () => {
+
+    return (
+        <div>
+            <h1></h1>
+            <iframe width="80%" height="800px" src="https://lookerstudio.google.com/embed/reporting/c30277fa-79d2-4f33-be99-643863594772/page/kdFrD" frameBorder={0} style={{border: '0px',}} allowFullScreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+        </div>
+    )
+}
+
+
+export default EmbeddedReport
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,8 @@
-import { useState, useEffect } from "react";
-import Head from "next/head";
-// import styles from "@/styles/Home.module.css";
-import {styled} from '@mui/material/styles'
-import {UserGroupIcon} from '@heroicons/react/24/solid'
-import {Box, Container, Unstable_Grid2 as Grid} from '@mui/material'
-import FarmersKpis from '../components/KpiCard'
-import Piechart from '../components/Piechart'
-import Linechart from '../components/Linechart'
-import Barchart from '../components/Barchart'
-import {CHART_PALLETE} from '../utils/chartPallete'
+import React from "react"
 
 
-export default function Home() {
-
+const Home = () => {
   return "Check /dashboard"
 }
+
+export default Home
